34a58a7cb4d2d85c52a319f97a71e7a7b1c30152
alphez <alphabrayen@gmail.com>
2025-11-22 21:35:54 +0300

    Update homepage layout

--- goldcinema_backend/build.sh
+++ goldcinema_backend/build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Move into backend folder
-cd goldcinema_backend
+pip install -r requirements.txt
 
-# Install requirements from the root directory
-pip install -r ../requirements.txt
-
-# Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
